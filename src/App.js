8fb03f96fa84621f988929a2d451da85281ddf89
hakmoune <mehdi.hakmoune@gmail.com>
2024-05-11 15:23:34 +0200

    React HOOK FROM WITH ZOD VALIDATION

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  firstname: z.string().includes("#"),
+  password: z.string().min(4),
+});
 
 function App() {
+  // register: to regdister the input with react hook form
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      firstname: "Firstname...",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+      throw new Error();
+    } catch (error) {
+      setError("root", {
+        message: "This firstname is already taken",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>useForm Hook</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="firstname">First Name</label>
+          <input type="text" id="firstname" {...register("firstname")} />
+        </div>
+        {errors.firstname && (
+          <div style={{ color: "red" }}>{errors.firstname.message}</div>
+        )}
+        <br />
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            autoComplete="on"
+            {...register("password")}
+          />
+        </div>
+        {errors.password && (
+          <div style={{ color: "red" }}>{errors.password.message}</div>
+        )}
+        <br />
+
+        <button type="submi" disabled={isSubmitting}>
+          {isSubmitting ? "Loading..." : "Submit"}
+        </button>
+
+        {errors.root && (
+          <div style={{ color: "red" }}>{errors.root.message}</div>
+        )}
+      </form>
     </div>
   );
 }
